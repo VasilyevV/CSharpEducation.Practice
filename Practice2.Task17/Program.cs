7683ac625151cbd4b64393d796979c59da118c47
VasilyevV <strang84@mail.ru>
2024-07-30 09:59:50 +0400

    add 17

--- Practice2.Task17/Program.cs
+++ Practice2.Task17/Program.cs
@@ -5,6 +5,18 @@ class Task17
 {
     static void Main()
     {
-
+        var rnd = new Random();
+        int a = rnd.Next(1, 100);
+        int b = rnd.Next(1, 100);
+        Console.WriteLine($"{a} {b}");
+        Task17.Replace( ref a, ref b);
+        Console.WriteLine($"{a} {b}");
+        Console.ReadLine();
+    }
+    static void Replace(ref int a, ref int b)
+    {
+        int t = a;
+        a = b;
+        b = t;
     }
 }
