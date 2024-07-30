3dbff4d1a587f2558bf8f4d2690446d2132ca513
VasilyevV <strang84@mail.ru>
2024-07-30 20:22:20 +0400

    add

---
+++ Practice2.Task18/Program.cs
@@ -0,0 +1,47 @@
+﻿// Создайте и реализуйте метод, который будет принимать массив на вход убирать
+//из него отрицательные числа и возвращать новый изменённый массив. Через
+//out параметр возвращать количество удалённых символов.
+using System;
+class Task18
+{
+    static void Main()
+    {
+        var rnd = new Random();
+        int[] ints = new int[10];
+        for(int i = 0; i < 10; i++) 
+            ints[i] = rnd.Next(-100, 100);
+        int index = 0;
+
+        var ints2 = Task18.DelNeg(ints, out index);
+
+        for (int i = 0; i < ints.Length; i++)
+            Console.Write($"{ints[i]} ");
+        Console.WriteLine();
+        for (int i = 0; i < ints2.Length; i++)
+            Console.Write($"{ints2[i]} ");
+        Console.WriteLine();
+        Console.WriteLine(index);
+
+        Console.ReadLine();
+    }
+
+    static int[] DelNeg(int[] array, out int index)
+    {
+        index = 0;
+        foreach (int i in array) 
+        {
+            if (i < 0)
+                index++;
+        }
+        int[] newarr = new int[array.Length - index];
+
+        for(int i = 0, j = 0; i < array.Length; i++, j++)
+        {
+            if (array[i] >= 0)
+                newarr[j] = array[i];
+            else
+                j--;
+        }
+        return newarr;
+    }
+}
---
+++ Practice2.Task19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Создайте одномерный массив целых чисел произвольной длины и заполните
+//случайными числами от 1 до 100. Выведите на экран разницу максимального и
+//минимального значений в нём.
+using System;
+class Task19
+{
+    static void Main()
+    {
+        var rnd = new Random();
+        int[] ints = new int[rnd.Next(10, 50)];
+
+        for (int i = 0; i < ints.Length; i++)
+            ints[i] = rnd.Next(1, 100);
+        foreach (int i in ints)
+            Console.Write($"{i} ");
+        Console.WriteLine();
+
+        int max = ints.Max();//простой вариант
+        int min = ints.Min();
+        Console.WriteLine($"{max}   {min}\nразница максимального и минимального значений: {max - min}");
+        Console.ReadLine();
+
+        max = ints[0];
+        for (int i = 1; i < ints.Length; i++)
+            if (ints[i] > max)
+                max = ints[i];
+        min = ints[0];
+        for (int i = 1; i < ints.Length; i++)
+            if (ints[i] < min)
+                min = ints[i];
+        Console.WriteLine($"{max}   {min}\nразница максимального и минимального значений: {max - min}");
+        Console.ReadLine();
+    }
+}
