24b13afc8febb7d20b4b121660ac0a310a71a88b
VasilyevV <strang84@mail.ru>
2024-07-30 09:42:27 +0400

    add

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿//Создайте и реализуйте метод, который будет принимать число по ссылке (ref) и
+//менять его знак на противоположный. Метод должен принимать один аргумент и
+//не возвращать ничего.
+using System;
+class Task16
+{
+    static void Main()
+    {
+        int a = 56;
+        Console.WriteLine(a);
+        Task16.Invert(ref a);
+        Console.WriteLine(a);
+
+        Console.ReadLine();
+    }
+    static void Invert(ref int a)
+    {
+        a = -a;
+    }
+}
