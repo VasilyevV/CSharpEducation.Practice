ea009c79e30e18b6520b4749aa7abfcb5f332ed1
VasilyevV <strang84@mail.ru>
2024-07-31 10:26:43 +0400

    add

--- Practice2.Task20/Program.cs
+++ Practice2.Task20/Program.cs
@@ -7,8 +7,32 @@ class Task20
     static void Main()
     {
         var rnd = new Random();
-        int[,] ints = new int[rnd.Next(10, 50), rnd.Next(10, 50)];
-        
+        int[,] ints = new int[10, 10];
+        for(int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                ints[i, j] = rnd.Next(1, 100);
 
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+                Console.Write($"{ints[i, j]} ");
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            int max = ints[i, 0];
+            int min = ints[i, 0];
+            for (int j = 0; j < 10; j++)
+            {
+                if (ints[i,j] < min)
+                    min = ints[i,j];
+                if (ints[i, j] > max)
+                    max = ints[i, j];
+            }
+            Console.WriteLine($"max = {max}  min = {min}, max - min = {max - min}");
+        }
+
+        Console.ReadLine();
     }
 }
---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите игру Угадай число. Программа случайно генерирует число от 1 до
+//100, а пользователь пытается угадать это число. При успешной догадке
+//выводите поздравление пользователя.
+class Task21
+{
+    static void Main()
+    {
+        var rnd = new Random();
+        int variable = rnd.Next(1, 100);
+
+        Console.WriteLine("Угадай число от 1 до 100");
+        Console.WriteLine("Вводите число");
+        int i = int.Parse(Console.ReadLine());
+
+        while (i != variable)
+        {
+            i = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Поздравляем, вы угадали!");
+
+        Console.ReadLine();
+    }
+}
