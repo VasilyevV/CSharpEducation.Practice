a8a7e8da5f1b9108587e46ef28872bc4ce9c768e
VasilyevV <strang84@mail.ru>
2024-07-31 11:42:07 +0400

    all

---
+++ Practice2.Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу для вычисления високосного года.
+//a.год, номер которого кратен 400, — високосный;
+//остальные годы, номер которых кратен 100, — невисокосные
+//остальные годы, номер которых кратен 4, — високосный;
+//все остальные годы — невисокосные.
+class Task23
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите год");
+        decimal year = decimal.Parse(Console.ReadLine());
+
+        if (year / 400 - Math.Truncate(year / 400) == 0)
+            Console.WriteLine("Это високосный год");
+        else if (year / 100 - Math.Truncate(year / 100) == 0)
+            Console.WriteLine("Это невисокосный год");
+
+        Console.ReadLine();
+
+        if (year / 4 - Math.Truncate(year / 4) == 0)
+            Console.WriteLine("Это високосный год");
+        else
+            Console.WriteLine("Это невисокосный год");
+
+        Console.ReadLine();
+    }
+}
---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Создайте перечисление (enum) для дней недели. Напишите программу, которая
+//выводит на экран дни недели по названиям
+class Task24
+{
+    enum Days
+    {
+        Понедельник,
+        Вторник,
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+    static void Main()
+    {
+        foreach(Days d in Enum.GetValues(typeof(Days)))
+            Console.WriteLine(d);
+
+        Console.ReadLine();
+    }
+}
